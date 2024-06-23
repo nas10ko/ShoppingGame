08f0cf15fad79c223c4c2a9edd32d8bd8ee8196d
HugTrees <141669276+nas10ko@users.noreply.github.com>
2024-06-22 19:08:26 -0500

    Switched to joystick controls

--- ShoppingGame/Assets/P1Move.cs
+++ ShoppingGame/Assets/P1Move.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class P1Move : MonoBehaviour
 {
+    public float sensX;
+    public float sensY;
+    float xRotation;
+    float yRotation;
+
     public float Speed;
     public Transform orientation; // Direction facing
 
@@ -16,16 +21,13 @@ public class P1Move : MonoBehaviour
 
 
 
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-    }
-
-    private void Player1_Inputs() {
-        HorizontalIn = Input.GetAxisRaw("Horizontal");
-        VertIn = Input.GetAxisRaw("Vertical");
     }
 
     private void Movement() { // Keyboard controls
@@ -36,10 +38,24 @@ public class P1Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxisRaw("Axis3") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Axis6") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, 0f, 0f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         Player1_Inputs();
     }
 
     private void FixedUpdate() {
         Movement();
+    }
+
+    public void Player1_Inputs() {
+        HorizontalIn = Input.GetAxisRaw("Horizontal");
+        VertIn = Input.GetAxisRaw("Vertical");
     }
 }
